85417aeb2dceb5a56a71b698906277ac4ca08079
Jacob Hendershott <jwhendershott@gmail.com>
2021-01-12 12:47:57 -0600

    formatted

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ $(document).ready(function () {
   var searchButton = $("#searchButton");
   searchButton.on("click", function () {
     
-    var queryURL =
-      "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=q8IV2ARGajfzJ3x9bFZ5INIEnegpbvk5";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=q8IV2ARGajfzJ3x9bFZ5INIEnegpbvk5";
 
     $.ajax({
       url: queryURL,
