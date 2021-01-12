4bbd73e89dc4a7c3ea30d22ab876ab228e6adf09
Jacob Hendershott <jwhendershott@gmail.com>
2021-01-12 12:44:51 -0600

    created ajax call/added api key

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
 
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=q8IV2ARGajfzJ3x9bFZ5INIEnegpbvk5"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+
 });
